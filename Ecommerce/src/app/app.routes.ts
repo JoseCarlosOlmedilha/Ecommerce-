d79c5d0ae7894058cc6a5f4313fdbc733be66cf4
JoseCarlosOlmedilha <jose.olmedilha@outlook.com>
2025-11-01 19:46:43 -0300

    Implementando o angular para o frontend

---
+++ Ecommerce/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { UsuarioCadastro } from './components/usuario-cadastro/usuario-cadastro';
+
+export const routes: Routes = [
+    {path: "usuario", component: UsuarioCadastro}
+];
+
---
+++ Ecommerce/src/app/components/usuario-cadastro/usuario-cadastro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-cadastro',
+  imports: [],
+  templateUrl: './usuario-cadastro.html',
+  styleUrl: './usuario-cadastro.scss',
+})
+export class UsuarioCadastro {
+
+}
